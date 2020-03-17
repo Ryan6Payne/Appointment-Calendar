574697d3c190147ab49ef84dc117e9f66639c939
Ryan <ryan6payne@gmail.com>
2020-03-17 12:41:04 +0000

    Firebase config added

---
+++ src/config/config.js
@@ -0,0 +1,20 @@
+import Firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATy1dg659roOErR-6AMv4lL8-rh4AQXp4",
+    authDomain: "appointment-calendar-1d076.firebaseapp.com",
+    databaseURL: "https://appointment-calendar-1d076.firebaseio.com",
+    projectId: "appointment-calendar-1d076",
+    storageBucket: "appointment-calendar-1d076.appspot.com",
+    messagingSenderId: "1059857522420",
+    appId: "1:1059857522420:web:e7f4ec6b77e5763ee888a1"
+};
+
+class FB {
+    constructor() {
+        Firebase.initializeApp(firebaseConfig);
+        this.db = Firebase.firestore();
+    }
+}
+
+export default new FB();
